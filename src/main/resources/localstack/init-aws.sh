3ed147cfc5fe213cbf441d0263a5d7c720529f42
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-12-08 11:06:25 +0800

    Updated localstack init

--- src/main/resources/localstack/init-aws.sh
+++ src/main/resources/localstack/init-aws.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 aws s3 --endpoint-url http://localhost:5001 mb s3://test-optout-bucket
-aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout/ s3://test-optout-bucket/ --recursive
+aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/ s3://test-optout-bucket/ --recursive
