122266f657988cf954c6ed8b48725cebd24b3a28
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-12-08 20:53:08 +0800

    Enhanced localstack init script

--- src/main/resources/localstack/init-aws.sh
+++ src/main/resources/localstack/init-aws.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 date="$(date '+%Y-%m-%d')"
-delta_dir="/s3/optout/optout-v2/delta/${date}"
-
 full_ts="$(date '+%Y-%m-%dT%H.%M.%SZ')"
 delta_file="optout-delta-000_${full_ts}_64692b14.dat"
 
-mkdir -p "$delta_dir"
-mv /s3/optout/optout-v2/delta/optout-delta-000.dat "${delta_dir}/${delta_file}"
 aws s3 --endpoint-url http://localhost:5001 mb s3://test-optout-bucket
-aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/ s3://test-optout-bucket/ --recursive
+aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout-v2/delta/2023-01-01/ s3://test-optout-bucket/optout-v2/delta/2023-01-01/ --recursive
+aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout-v2/delta/optout-delta-000.dat "s3://test-optout-bucket/optout-v2/delta/${date}/${delta_file}" --recursive
