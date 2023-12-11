3d1305821866839907dc5039dd351bcfebc0e765
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-12-11 11:06:39 +0800

    Removed --recursive for single file copy

--- src/main/resources/localstack/init-aws.sh
+++ src/main/resources/localstack/init-aws.sh
@@ -6,4 +6,4 @@ delta_file="optout-delta-000_${full_ts}_64692b14.dat"
 
 aws s3 --endpoint-url http://localhost:5001 mb s3://test-optout-bucket
 aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout-v2/delta/2023-01-01/ s3://test-optout-bucket/optout-v2/delta/2023-01-01/ --recursive
-aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout-v2/delta/optout-delta-000.dat "s3://test-optout-bucket/optout-v2/delta/${date}/${delta_file}" --recursive
+aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout-v2/delta/optout-delta-000.dat "s3://test-optout-bucket/optout-v2/delta/${date}/${delta_file}"
