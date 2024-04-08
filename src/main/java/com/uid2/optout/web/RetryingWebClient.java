8ca7aa528f08bf76d9133a1f9b3aa03f8b121a6a
Matt Collins <matt.collins@thetradedesk.com>
2024-04-08 13:46:17 +1000

    Add more logging around HTTP request

--- src/main/java/com/uid2/optout/web/RetryingWebClient.java
+++ src/main/java/com/uid2/optout/web/RetryingWebClient.java
@@ -1,5 +1,6 @@
 package com.uid2.optout.web;
 
+import com.google.common.base.Stopwatch;
 import io.netty.handler.codec.http.HttpMethod;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
@@ -10,7 +11,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.BiFunction;
 
@@ -34,15 +37,25 @@ public class RetryingWebClient {
     }
 
     public Future<Void> send(BiFunction<URI, HttpMethod, HttpRequest> requestCreator, Function<HttpResponse, Boolean> responseValidator) {
-        return this.send(requestCreator, responseValidator, 0);
+        final UUID requestId = UUID.randomUUID();
+        return this.send(requestCreator, responseValidator, 0, requestId)
+                .onFailure(ex -> LOGGER.error("requestId={} Request to {} failed", requestId, uri, ex));
     }
 
-    public Future<Void> send(BiFunction<URI, HttpMethod, HttpRequest> requestCreator, Function<HttpResponse, Boolean> responseValidator, int currentRetries) {
+    public Future<Void> send(BiFunction<URI, HttpMethod, HttpRequest> requestCreator, Function<HttpResponse, Boolean> responseValidator, int currentRetries, UUID requestId) {
         HttpRequest request = requestCreator.apply(this.uri, this.method);
+
+        LOGGER.info("requestId={} Sending request to {}, currentRetries={}", requestId, uri, currentRetries);
+
+        final Stopwatch sw = Stopwatch.createStarted();
 
         final CompletableFuture<HttpResponse<Void>> asyncResponse = this.httpClient.sendAsync(request, HttpResponse.BodyHandlers.discarding());
 
         return Future.fromCompletionStage(asyncResponse, vertx.getOrCreateContext()).compose(response -> {
+            sw.stop();
+
+            LOGGER.info("requestId={} Request to {} completed in {}ms, currentRetries={}, status={}, version={}", requestId, uri, sw.elapsed(TimeUnit.MILLISECONDS), currentRetries, response.statusCode(), response.version());
+
             Boolean responseOK = responseValidator.apply(response);
             if (responseOK == null) {
                 return Future.failedFuture(new RuntimeException("Response validator returned null"));
@@ -53,16 +66,16 @@ public class RetryingWebClient {
             }
 
             if (currentRetries < this.retryCount) {
-                LOGGER.error("failed sending to " + uri + ", currentRetries: " + currentRetries + ", backing off before retrying");
+                LOGGER.error("requestId={} failed sending to {}, currentRetries={}, backing off for {}ms before retrying", requestId, uri, currentRetries, this.retryBackoffMs);
                 if (this.retryBackoffMs > 0) {
                     return vertx.timer(this.retryBackoffMs)
-                            .compose(v -> send(requestCreator, responseValidator, currentRetries + 1));
+                            .compose(v -> send(requestCreator, responseValidator, currentRetries + 1, requestId));
                 } else {
-                    return send(requestCreator, responseValidator, currentRetries + 1);
+                    return send(requestCreator, responseValidator, currentRetries + 1, requestId);
                 }
             }
 
-            LOGGER.error("retry count exceeded for sending to " + this.uri);
+            LOGGER.error("requestId={} retry count exceeded for sending to {}", requestId, this.uri);
             return Future.failedFuture(new TooManyRetriesException(currentRetries));
         });
     }
