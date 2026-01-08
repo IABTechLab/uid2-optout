ab30918efeac26195abca938da6f3096ba2035c8
Ian-Nara <ian.nara@thetradedesk.com>
2026-01-08 14:08:31 -0700

    e2e config test

--- src/main/resources/localstack/init-aws.sh
+++ src/main/resources/localstack/init-aws.sh
@@ -7,3 +7,5 @@ delta_file="optout-delta-000_${full_ts}_64692b14.dat"
 aws s3 --endpoint-url http://localhost:5001 mb s3://test-optout-bucket
 aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout-v2/delta/2023-01-01/ s3://test-optout-bucket/optout-v2/delta/2023-01-01/ --recursive
 aws s3 --endpoint-url http://localhost:5001 cp /s3/optout/optout-v2/delta/optout-delta-000.dat "s3://test-optout-bucket/optout-v2/delta/${date}/${delta_file}"
+
+aws sqs --endpoint-url http://localhost:5001 create-queue --queue-name optout-queue
