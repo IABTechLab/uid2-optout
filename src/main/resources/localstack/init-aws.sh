04ae551106f77d5caa811d700486e9e9d4d4b69d
Ian-Nara <ian.nara@thetradedesk.com>
2026-01-20 18:26:13 -0700

    clean

--- src/main/resources/localstack/init-aws.sh
+++ src/main/resources/localstack/init-aws.sh
@@ -9,21 +9,10 @@ date="$(date '+%Y-%m-%d')"
 full_ts="$(date '+%Y-%m-%dT%H.%M.%SZ')"
 delta_file="optout-delta-000_${full_ts}_64692b14.dat"
 
-echo "Creating S3 bucket..."
 awslocal s3 mb s3://test-optout-bucket || echo "Bucket may already exist"
 
-echo "Copying delta files to S3..."
 awslocal s3 cp /s3/optout/optout-v2/delta/2023-01-01/ s3://test-optout-bucket/optout-v2/delta/2023-01-01/ --recursive
 awslocal s3 cp /s3/optout/optout-v2/delta/optout-delta-000.dat "s3://test-optout-bucket/optout-v2/delta/${date}/${delta_file}"
 
-echo "Creating SQS queue..."
 awslocal sqs create-queue --queue-name optout-queue
 echo "Queue creation exit code: $?"
-
-echo "Verifying SQS queue..."
-awslocal sqs get-queue-url --queue-name optout-queue
-
-echo "Listing all queues..."
-awslocal sqs list-queues
-
-echo "Optout LocalStack initialization complete."
